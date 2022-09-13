8e6f3a3d0a6087ee433efb1a0a27cd8c091b6190
Mohammed chahboun <m97.chahboun@gmail.com>
2022-09-13 23:06:48 +0100

    merge dev branch

--- android/app/src/main/kotlin/com/example/flutter_ci_cd/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_ci_cd
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/flutter/cicd/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutter.cicd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
