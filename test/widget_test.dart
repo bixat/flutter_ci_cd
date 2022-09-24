9d6520b821766488c2a86c23818ac81ed3b1b15c
Mohammed chahboun <m97.chahboun@gmail.com>
2022-09-24 22:55:09 +0100

    format tests code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_ci_cd/main.dart';
 
 void main() {
-  testWidgets('Counter increments & decrement smoke test', (WidgetTester tester) async {
+  testWidgets('Counter increments & decrement smoke test',
+      (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
