08cbb57824131d7a90282494564af4f2fd251576
Mohammed  CHAHBOUN <69054810+M97Chahboun@users.noreply.github.com>
2022-09-24 23:01:11 +0100

    Add decrement count button (#7)
    * Add decrement ui & logic
    
    * Add test for decrement
    
    * format code
    
    * add decrement name for test title
    
    * format tests code
    
    * Fix test command on dev workflow

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_ci_cd/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Counter increments & decrement smoke test',
+      (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
@@ -26,5 +27,13 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
+
+    // Tap the '-' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.minimize));
+    await tester.pump();
+
+    // Verify that our counter has decremented.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
   });
 }
