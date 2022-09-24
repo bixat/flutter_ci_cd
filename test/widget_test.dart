45deb5a54a6bd32fd084e11a513e49df8d0eec12
Mohammed chahboun <m97.chahboun@gmail.com>
2022-09-24 22:38:53 +0100

    add decrement name for test title

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_ci_cd/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Counter increments & decrement smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
