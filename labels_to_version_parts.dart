42cdf503a451a2bdb882488d56f148324561b3e1
Mohammed chahboun <m97.chahboun@gmail.com>
2022-09-30 00:06:35 +0100

    Control deploy track from labels for staging workflow

--- labels_to_version_parts.dart
+++ labels_to_version_parts.dart
@@ -2,5 +2,9 @@ const List<String> options = ["major", "minor", "patch"];
 void main(List<String> labels) {
   List<String> versionParts = List.from(labels);
   versionParts.removeWhere((e) => !options.contains(e));
-  print("::set-output name=parts::bump:${versionParts.join(",")}");
+  String parts = "alpha";
+  if (versionParts.isNotEmpty) {
+    parts = versionParts.join(",");
+  }
+  print("::set-output name=parts::bump:$parts");
 }
