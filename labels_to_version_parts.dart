5c6b4e2e1eead4b5aeedbaa1be0341e340e8b03b
Mohammed chahboun <m97.chahboun@gmail.com>
2022-09-30 20:17:29 +0100

    Change default value of version parts to empty

--- labels_to_version_parts.dart
+++ labels_to_version_parts.dart
@@ -2,9 +2,10 @@ const List<String> options = ["major", "minor", "patch"];
 void main(List<String> labels) {
   List<String> versionParts = List.from(labels);
   versionParts.removeWhere((e) => !options.contains(e));
-  String parts = "alpha";
+  String parts = "";
   if (versionParts.isNotEmpty) {
     parts = versionParts.join(",");
+    parts = "bump:$parts";
   }
-  print("::set-output name=parts::bump:$parts");
+  print("::set-output name=parts::$parts");
 }
