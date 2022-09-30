da9042f328fdf0e6c9e026067832847f3ac14b62
Mohammed  CHAHBOUN <69054810+M97Chahboun@users.noreply.github.com>
2022-09-30 20:44:24 +0100

    Test All Workflows (#17)
    * WIP: add commit
    
    * Removed check last commit workflow
    
    * Enhanced deploy input workflow
    
    * Add multi-bump version example on deploy woekflow
    
    * Add bump version by label in dev PR
    
    * Check bump version from labels (#10)
    
    * Add bump version by label in dev PR
    
    * Run dev workflow in PR
    
    * Add Bump app version based on PR labels workflow
    
    * Check Bump version based on PR labels (#11)
    
    * update readme
    
    * Disabled dev ci & remove condition on bump version
    
    * Test bump version based on labels and push tags (#12)
    
    * update readme
    
    * Disabled dev ci & remove condition on bump version
    
    * Push tags after bump version
    
    * Test bump version based on labels + Enable push tag (#13)
    
    * update readme
    
    * Disabled dev ci & remove condition on bump version
    
    * Push tags after bump version
    
    * Enhance labeles to version parts workflow
    
    * Disabled build auto-increase for dev bump version workflow
    
    * Change app bar color (#14)
    
    * Add primary color
    
    * Change app bar to primary color
    
    * Enable dev workflow
    
    * Removed bump version by labels job from dev workflow
    
    * Disabled bump version from staging workflow
    
    * Convert stabel workflow from deploy to promote for use staging version
    
    * Control deploy track from labels for staging workflow
    
    * Change staging workflow trigger to closed PR
    
    * Change default value of version parts to empty
    
    * Add create release from last tag in stable workflow
    
    * update readme

--- labels_to_version_parts.dart
+++ labels_to_version_parts.dart
@@ -2,5 +2,10 @@ const List<String> options = ["major", "minor", "patch"];
 void main(List<String> labels) {
   List<String> versionParts = List.from(labels);
   versionParts.removeWhere((e) => !options.contains(e));
-  print("::set-output name=parts::bump:${versionParts.join(",")}");
+  String parts = "";
+  if (versionParts.isNotEmpty) {
+    parts = versionParts.join(",");
+    parts = "bump:$parts";
+  }
+  print("::set-output name=parts::$parts");
 }
