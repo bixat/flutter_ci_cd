7e22fdebee9d566fc8cbc59bc5cb2098b2bb1a99
Mohammed <m97.chahboun@gmail.com>
2022-10-15 18:57:36 +0100

    migrate labels_to_track

--- workflows_utils/labels_to_track.dart
+++ workflows_utils/labels_to_track.dart
@@ -1,10 +1,13 @@
+import 'dart:io';
+
 const List<String> options = ["alpha", "beta", "internal"];
-void main(List<String> labels) {
+Future<void> main(List<String> labels) async {
   List<String> trackLabels = List.from(labels);
   trackLabels.removeWhere((e) => !options.contains(e));
   String track = "alpha";
   if (trackLabels.isNotEmpty) {
     track = trackLabels.first;
   }
-  print("::set-output name=track::$track");
+  await Process.run('echo "track=$track" >> \$GITHUB_ENV', [],
+      runInShell: true);
 }
