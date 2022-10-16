a217434654ee66a224e68dd47d06f9d4dd1fed7e
Mohammed <m97.chahboun@gmail.com>
2022-10-16 12:55:56 +0100

    Use env keyword in workflow

--- workflows_utils/add_to_output.dart
+++ workflows_utils/add_to_output.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 Future<void> setOutput(String item) async {
   Map<String, String> envVars = Platform.environment;
-  String? envPath = envVars["GITHUB_OUTPUT"];
+  String? envPath = envVars["GITHUB_ENV"];
   File ghEnv = File(envPath!);
   String currentContent = await ghEnv.readAsString();
   if (currentContent.isNotEmpty) {
