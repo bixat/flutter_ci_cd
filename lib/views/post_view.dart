6666dee462afe97767a29344a43d8896fe7be4b2
Mohammed  CHAHBOUN <69054810+M97Chahboun@users.noreply.github.com>
2022-10-21 17:52:21 +0100

    Add posts mvc rocket example (#101)
    * Add post model
    
    * Add post request
    
    * Add post view
    
    * Integrate posts feature
    
    * bump version from 2.4.0+22 to 2.5.0+22 by fastlane plugin

---
+++ lib/views/post_view.dart
@@ -0,0 +1,125 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_ci_cd/models/post_model.dart';
+import 'package:flutter_ci_cd/requests/post_request.dart';
+import 'package:mvc_rocket/mvc_rocket.dart';
+
+class PostExample extends StatelessWidget {
+  // Save your model to use on another screen
+  // readOnly means if you close and open this screen you will use same data without update it from Api
+  // [rocket] is instance of Mccontroller injected in Object by extension for use it easily anywhere
+  final Post post = Rocket.add<Post>(postsEndpoint, Post(), readOnly: true);
+
+  PostExample({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Refresh Posts with swip to down or from here =>",
+            style: TextStyle(fontSize: 11.0),
+          ),
+          actions: [
+            IconButton(
+                icon: const Icon(Icons.data_usage),
+                // Refresh Data from Api
+                onPressed: () => GetPosts.getPosts(post))
+          ],
+        ),
+        body: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: RefreshIndicator(
+              onRefresh: () {
+                return GetPosts.getPosts(post);
+              },
+              child: RocketView(
+                // call api method
+                call: () => GetPosts.getPosts(post),
+                // your model generated
+                model: post,
+                // call call Voidcallback if model empty
+                callType: CallType.callIfModelEmpty,
+                // or
+                // callType: CallType.callAsStream,
+                // secondsOfStream: 1,
+                // customized your loading (default widget is CircularProgressIndicator)
+                // loader:CustomLoading(),
+
+                // handle errors
+                onError: (RocketException exception, Function() reload) {
+                  return Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(exception.exception),
+                        if (exception.statusCode != HttpStatus.ok) ...[
+                          Text(exception.response),
+                          Text(Rocket.get(rocketRequestKey)
+                              .msgByStatusCode(exception.statusCode))
+                        ],
+                        TextButton(
+                            onPressed: reload, child: const Text("retry"))
+                      ],
+                    ),
+                  );
+                },
+                builder: (context) {
+                  return SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.852,
+                    child: ListView.builder(
+                      itemCount: post.multi == null ? 5 : post.multi!.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        // your data saved in multi list as Post model
+                        Post currentPost = post.multi == null
+                            ? Post(userId: 0, title: "loading", body: "loading")
+                            : post.multi![index];
+                        return ListTile(
+                            leading: Text(currentPost.id.toString()),
+                            title: Text(currentPost.title!),
+                            trailing: IconButton(
+                              color: Colors.brown,
+                              icon: const Icon(Icons.update),
+                              onPressed: () {
+                                // update post data
+                                currentPost.updateFields(
+                                    titleField: "This Title changed");
+                              },
+                            ),
+                            onTap: () => Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                      builder: (BuildContext context) {
+                                    return Details(index);
+                                  }),
+                                ));
+                      },
+                    ),
+                  );
+                },
+              )),
+        ));
+  }
+}
+
+class Details extends StatelessWidget {
+  final int index;
+  //  get your model by key
+  final Post post = Rocket.get<Post>(postsEndpoint);
+  Details(this.index, {Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    Post currentPost = post.multi![index];
+    return Scaffold(
+      appBar: AppBar(title: Text(currentPost.title!)),
+      body: Center(
+        child: ListTile(
+          leading: Text(currentPost.id.toString()),
+          title: Text(currentPost.title!),
+          subtitle: Text(currentPost.body!),
+        ),
+      ),
+    );
+  }
+}
