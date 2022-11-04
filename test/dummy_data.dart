43d356a3553763df78f55c24d3c5984899345ec9
Mohammed <m97.chahboun@gmail.com>
2022-11-04 22:24:13 +0100

    Add dummy data & fake rocket request object

---
+++ test/dummy_data.dart
@@ -0,0 +1,37 @@
+List<Map<String, dynamic>> photoData = [
+  {
+    "albumId": 1,
+    "id": 1,
+    "title": "accusamus beatae ad facilis cum similique qui sunt",
+    "url": "https://via.placeholder.com/600/92c952",
+    "thumbnailUrl": "https://via.placeholder.com/150/92c952"
+  },
+];
+
+List<Map<String, dynamic>> postData = [
+  {"userId": 1, "id": 1, "title": "title", "body": "body"}
+];
+
+List<Map<String, dynamic>> userData = [
+  {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {"lat": "-37.3159", "lng": "81.1496"}
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    },
+    "image": null
+  }
+];
