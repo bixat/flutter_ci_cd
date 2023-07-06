754dcdd09fb637782799642b8e3035e05c0efb4b
Mohammed <m97.chahboun@gmail.com>
2023-07-06 15:45:24 +0100

    update flutter rocket example

--- lib/requests/photo_request.dart
+++ lib/requests/photo_request.dart
@@ -1,9 +1,10 @@
 import 'package:flutter_ci_cd/models/photo_model.dart';
-import 'package:mvc_rocket/mvc_rocket.dart';
+import 'package:flutter_rocket/flutter_rocket.dart';
+
 
 const String photosEndpoint = "photos";
 
 class GetPhotos {
-  static Future getPhotos(Photo photoModel) => Rocket.get(rocketRequestKey)
-      .getObjData(photosEndpoint, photoModel, multi: true);
+  static Future getPhotos(Photo photoModel) =>
+      Rocket.get<RocketClient>().request(photosEndpoint, model: photoModel);
 }
