4033e9862d19c4445372f8da8c5aefa32657b170
Mohammed  CHAHBOUN <69054810+M97Chahboun@users.noreply.github.com>
2023-07-06 11:05:14 -0700

    update ci/cd workflows (#121)
    * update dev workflow
    
    * Update workflows
    
    * fixed test issue
    
    * Support version name & code
    
    * trigger lib path on dev workflow
    
    * updated deploy and stable workflows
    
    * Added tests path to dev workflow

--- workflows_utils/get_project_version.dart
+++ workflows_utils/get_project_version.dart
@@ -7,6 +7,13 @@ main() async {
   String text = data.readAsStringSync();
   String currentVersion = text.substring(text.indexOf("version: ") + 9,
       text.indexOf("\n", text.indexOf("version: ")));
-  String versionEnvVar = "project_version=v${currentVersion.trim()}";
+  String versionName = currentVersion;
+  String versionCode = '1';
+  if (currentVersion.contains('+')) {
+    versionName = currentVersion.split('+').first;
+    versionCode = currentVersion.split('+').last;
+  }
+  String versionEnvVar =
+      "version_name=${versionName.trim()}\nversion_code=${versionCode.trim()}";
   await setOutput(versionEnvVar);
 }
